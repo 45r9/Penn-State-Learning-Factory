31476a2718b79ba971c3c7ff4161437239e6c616
prateekchandra98 <40297601+prateekchandra98@users.noreply.github.com>
2018-11-27 12:46:17 -0500

    home

--- demo/src/main/java/demos/gui/uicomponents/HomePageController.java
+++ demo/src/main/java/demos/gui/uicomponents/HomePageController.java
@@ -8,7 +8,11 @@ package demos.gui.uicomponents;
 import io.datafx.controller.ViewController;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javax.annotation.PostConstruct;
 
 /**
  * FXML Controller class
@@ -18,7 +22,25 @@ import javafx.fxml.Initializable;
 @ViewController(value = "/fxml/ui/HomePage.fxml", title = "Material Design Example")
 public class HomePageController {
 
+    @FXML
+    private LineChart<String,Number> graphview;
     /**
      * Initializes the controller class.
      */
+    private void loadgraph() {
+
+        XYChart.Series<String,Number> series = new XYChart.Series<String,Number>();
+        graphview.getData().clear();
+        series.setName("RFID DATA");
+        series.getData().add(new XYChart.Data<String,Number>("11-9", 11));
+        series.getData().add(new XYChart.Data<String,Number>("11-12", 22));
+        series.getData().add(new XYChart.Data<String,Number>("11-13", 18));
+        series.getData().add(new XYChart.Data<String,Number>("11-14", 7));
+        graphview.getData().add(series);
+
+     }
+    @PostConstruct
+    public void init() {
+        loadgraph();
+    }
 }
